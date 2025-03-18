5c1cac200eb5900757b98d75a89759123bb2bb2c
Sergey Diachkov <biometer2020@gmail.com>
2025-03-18 16:25:23 +0300

    Initial commit

---
+++ src/main/java/solutions/brilliant/proceduralGeneration/ProceduralGeneration.java
@@ -0,0 +1,17 @@
+package solutions.brilliant.proceduralGeneration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProceduralGeneration extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
