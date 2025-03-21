2228133229b32fce4800812881f7a077ff9591a1
Sergey Diachkov <biometer2020@gmail.com>
2025-03-21 16:29:04 +0300

    Add main command, executor and tab completer

--- src/main/java/solutions/brilliant/proceduralGeneration/ProceduralGeneration.java
+++ src/main/java/solutions/brilliant/proceduralGeneration/ProceduralGeneration.java
@@ -1,17 +1,22 @@
 package solutions.brilliant.proceduralGeneration;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import solutions.brilliant.proceduralGeneration.commands.BSGCommandHandler;
+
+import java.util.logging.Level;
 
 public final class ProceduralGeneration extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().log(Level.INFO, "Plugin startup");
+        saveDefaultConfig();
+        getCommand("bs-generator").setExecutor(new BSGCommandHandler());
+        getCommand("bs-generator").setTabCompleter(new BSGCommandHandler());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().log(Level.INFO, "Plugin disabled");
     }
 }
---
+++ src/main/java/solutions/brilliant/proceduralGeneration/commands/CreateGame.java
@@ -0,0 +1,4 @@
+package solutions.brilliant.proceduralGeneration.commands;
+
+public class CreateGame {
+}
